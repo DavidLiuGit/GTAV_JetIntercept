f8979e8bfbfbca14f7b371e11b78c03bcdf0f960
David Liu <dvdgege5@gmail.com>
2020-04-08 16:38:39 -0700

    remove template code; rename output file

--- AirTraffic/Main.cs
+++ AirTraffic/Main.cs
@@ -1,14 +1,16 @@
-﻿// AirTraffic 1.0 - Abel Software
-// You must download and use Scripthook V Dot Net Reference (LINKS AT BOTTOM OF THE TEMPLATE)
+﻿// You must download and use Scripthook V Dot Net Reference (LINKS AT BOTTOM OF THE TEMPLATE)
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 using GTA;
 using GTA.Native;
 using GTA.Math;
+using GTA.UI;
+
 
 namespace ModName // !!!! IMPORTANT REPLACE THIS WITH YOUR MODS NAME !!!!
 {
@@ -18,7 +20,6 @@ namespace ModName // !!!! IMPORTANT REPLACE THIS WITH YOUR MODS NAME !!!!
 		bool firstTime = true;
 		string ModName = "MOD NAME";
 		string Developer = "YOUR NAME";
-		string Version = "1.0";
 
 		public Main()
 		{
@@ -31,7 +32,7 @@ namespace ModName // !!!! IMPORTANT REPLACE THIS WITH YOUR MODS NAME !!!!
 		{
 			if (firstTime) // if this is the users first time loading the mod, this information will appear
 			{
-				UI.Notify(ModName + " " + Version + " by " + Developer + " Loaded");
+				Notification.Show(ModName + " by " + Developer + " Loaded");
 				firstTime = false;
 			}
 			// If the user has used the current mod version before, the text (and code) above will not appear
@@ -46,21 +47,6 @@ namespace ModName // !!!! IMPORTANT REPLACE THIS WITH YOUR MODS NAME !!!!
 
 		private void onKeyDown(object sender, KeyEventArgs e)
 		{
-			// Let's begin with a simple keydown to perform an action
-
-			// In this code sample, if the player has a wanted level, it will clear it. If they don't, text will appear.
-			if (e.KeyCode == Keys.F3) // The code below will begin once the player hits F3
-			{
-				if (Game.Player.WantedLevel == 0) // If the player has no wanted level, the text below will appear in red.
-				{
-					UI.ShowSubtitle("~r~You have no wanted levels!"); // Informs player they have no wanted level
-				}
-				else //However, if the player does have a wanted level, the wanted level will be removed.
-				{
-					Game.Player.WantedLevel = 0; // Removes wanted level (or resets it to 0)
-				}
-			}
-			// This is the end of the F3 code, we can now place another key press below.
 
 		}
 
